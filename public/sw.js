65bc4bdfdbe58c97a9e600d023ce214e60104d24
Puncudo <shaydo2057@gmail.com>
2026-04-26 17:25:02 +0300

    Fix service worker: clear old caches on activate

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,13 @@
+const CACHE_VERSION = 'v2'
+
 self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()))
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  )
+})
+
 self.addEventListener('fetch', () => {})
